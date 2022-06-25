0920d60f3c6669804235393dfa1e3c2aae1c8c0d
SandhyaSuresh06 <sandhyasuresh@oakland.edu>
2022-06-25 00:39:01 -0400

    streamlit changes

--- notebooks/patent_barchart.py
+++ notebooks/patent_barchart.py
@@ -11,7 +11,44 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
-patent_details_text = open('output/patent_details.json').read()
+#List of response fields
+response_fields_list = [ "patent_firstnamed_assignee_city", "patent_firstnamed_assignee_country",
+                        "patent_firstnamed_assignee_id", "patent_firstnamed_assignee_latitude", 
+                        "patent_firstnamed_assignee_longitude","patent_firstnamed_assignee_state", 
+                        "patent_firstnamed_inventor_city", "patent_firstnamed_inventor_country",
+                        "patent_firstnamed_inventor_id", "patent_firstnamed_inventor_latitude", 
+                        "patent_firstnamed_inventor_longitude", "patent_firstnamed_inventor_state",
+                        "patent_num_cited_by_us_patents", "patent_number", "patent_title", 
+                        "patent_type", "patent_year", "patent_date"]
+#Filters for the results
+patent_inv_country = "US"
+patent_assignee_country = "US"
+
+
+# Form the final url
+base_url = 'https://api.patentsview.org/patents/query?q={"_gte":{'
+parameter_1 = f'"patent_firstnamed_inventor_country":"{patent_inv_country}"'
+parameter_2 = f'"patent_firstnamed_assignee_country":"{patent_assignee_country}"'
+response_fields = '}}&f=['
+
+url = base_url + parameter_1 + "," + parameter_2 + response_fields
+final_url = url
+
+for i in range(0, len(response_fields_list)):
+    final_url = final_url + '"' + response_fields_list[i] + '",'
+    #print(final_url)
+final_url = final_url[:-1:]
+final_url = final_url + ']' + '&o={"per_page": 10000} &s=[{"patent_number":"asc"}]'
+
+print(final_url)
+
+payload={}
+
+response = requests.request("GET", final_url, data=payload)
+
+#print(response.text)
+
+patent_details_text = response.text
 
 #Load patent data from the patent_details.json file
 patent_details = json.loads(patent_details_text)
